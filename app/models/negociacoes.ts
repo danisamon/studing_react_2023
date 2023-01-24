40c345096c8a2334b57816963d4410dafd66fed7
Danielle Monteiro <danisamon@gmail.com>
2023-01-24 19:36:23 -0300

    Retirada de métodos getters e utilização do modificador readonly

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes{
-    private negociacoes: Array<Negociacao> = []
+    //private negociacoes: Array<Negociacao> = []
+    private negociacoes: Negociacao[] = []
     
     adicionar(negociacao: Negociacao){
         this.negociacoes.push(negociacao)
     }
-    listar(): ReadonlyArray<Negociacao>{
+    //listar(): ReadonlyArray<Negociacao>{ outra forma utilizadndo o modificador readonly
+    listar(): readonly Negociacao[]{
 
         //return [...this.negociacoes]
         return this.negociacoes
