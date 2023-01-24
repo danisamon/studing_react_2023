1d62182aade6e4b1da7cd32ee29a252a6bbd3355
Danielle Monteiro <danisamon@gmail.com>
2023-01-24 00:20:31 -0300

    adicionando typescript no projeto e as regras de compilação

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+import { Negociacao } from "./models/negociacoes.js";
+
+const negociacao = new Negociacao(new Date(), 10, 200)
+console.log(negociacao)
+//negociacao.quantidade = 1000
+console.log(negociacao.volume)
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,5 @@
 import { Negociacao } from "./models/negociacoes.js";
-
-const negociacao = new Negociacao(new Date(), 10, 100)
-console.log(negociacao)
+const negociacao = new Negociacao(new Date(), 10, 200);
+console.log(negociacao);
 //negociacao.quantidade = 1000
-console.log(negociacao.volume)
+console.log(negociacao.volume);
