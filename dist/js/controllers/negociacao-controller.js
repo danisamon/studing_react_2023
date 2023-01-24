4136047fb926213b7341bed09ddd8acda2166aa5
Danielle Monteiro <danisamon@gmail.com>
2023-01-24 01:07:50 -0300

    adicionando tipagem nas variáveis e nos retornos das funções

---
+++ dist/js/controllers/negociacao-controller.js
@@ -0,0 +1,20 @@
+import { Negociacao } from "../models/negociacoes.js";
+export class NegociacaoController {
+    constructor() {
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
+    }
+    adiciona() {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+    criaNegociacao() {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        const negociacao = new Negociacao(date, quantidade, valor);
+        return negociacao;
+    }
+}
